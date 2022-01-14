4301eea98e7b15cfda4c226da36b7bf1615a91bd
bae hyoseong <bhsbhs235@naver.com>
2022-01-14 23:12:05 +0900

    회원가입(테스트 전) - typescript오류 수정

--- BE/src/config/properties/properties.ts
+++ BE/src/config/properties/properties.ts
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-    port: parseInt(process.env.PORT!, 10),
+    port: process.env.PORT || 5000,
 
-    mongoURL: process.env.MONGO_URL,
+    mongoURL: process.env.MONGO_URL || "",
 
-    jwtSecret: process.env.JWT_SECRET,
-    jwtAlgorithm: process.env.JWT_ALGO,
+    jwtSecret: process.env.JWT_SECRET || "",
+    jwtAlgorithm: process.env.JWT_ALGO || "",
 
 };
