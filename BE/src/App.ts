5a5f769f4d047e3b8afe8b165988c644ce8d717b
hyoseong Bae <bhsbhs235@naver.com>
2022-01-16 21:32:18 +0900

    complete CORS setting

--- BE/src/App.ts
+++ BE/src/App.ts
@@ -17,7 +17,13 @@ app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+
+//CORS 설정
+const coreOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+};
+app.use(cors(coreOptions));
 
 app.use('/', indexRouter);
 
