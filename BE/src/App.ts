8008008abb76a58b7f4ab6ad5d455aa74e7db9b4
배효성 <baehyoseong@baehyoseong-ui-MacBookPro.local>
2022-01-16 18:36:32 +0900

    Add cors

--- BE/src/App.ts
+++ BE/src/App.ts
@@ -17,7 +17,7 @@ app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
 
 app.use('/', indexRouter);
 
