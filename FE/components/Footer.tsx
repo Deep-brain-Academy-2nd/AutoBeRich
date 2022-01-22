76ba62d1ca424e020175d2ec4cbd87ab220c266f
gimdongwon <ehddnjs8989@gmail.com>
2022-01-22 14:35:52 +0900

    Edit footer, account's styling

--- FE/components/Footer.tsx
+++ FE/components/Footer.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
+import Link from 'next/link';
 
 const FooterPart = styled.footer`
   width: 100%;
   height: 6%;
-  font-size: 3rem;
+  font-size: 2rem;
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: gray;
 `;
 
 function Footer() {
-  return <FooterPart>Footer</FooterPart>;
+  return (
+    <FooterPart>
+      <p>{'Copyright © for AutoBeRich '}</p>
+      AI Deepbrain by Dongwon & hyosung
+    </FooterPart>
+  );
 }
 
 export default Footer;
--- FE/components/panners/MyAccount.tsx
+++ FE/components/panners/MyAccount.tsx
@@ -9,7 +9,20 @@ const MyAccountWrap = Styled.div`
 `;
 
 const MyAccount = (userInfo) => {
-  return <MyAccountWrap>MyAccountWrap</MyAccountWrap>;
+  return (
+    <MyAccountWrap>
+      <ul>
+        <li>보유원화</li>
+        <li>평가금액</li>
+        <li>선택 전략</li>
+        <li>수익률</li>
+        <li>보유 코인</li>
+        <li>보유 수량</li>
+        <li>매수금액</li>
+        <li>매수 평균가</li>
+      </ul>
+    </MyAccountWrap>
+  );
 };
 
 export default MyAccount;
