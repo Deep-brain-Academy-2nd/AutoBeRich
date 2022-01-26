8b67f8c396362367612b4bec695cac0a8ed0ba55
gimdongwon <ehddnjs8989@gmail.com>
2022-01-26 16:57:40 +0900

    Add hpp, helmet

--- BE/src/App.ts
+++ BE/src/App.ts
@@ -7,6 +7,8 @@ import properties from './config/properties/properties';
 import cors from 'cors';
 import sanitizeHTML from 'sanitize-html';
 import { logger } from './loaders/logger';
+import hpp from 'hpp';
+import helmet from 'helmet';
 
 connectDB();
 //require('dotenv').config();
@@ -17,7 +19,9 @@ app.set('port', properties.port || 5000);
 
 // 사용자 정보 남기기 위한 morgan
 if (properties.NODE_ENV === 'production') {
-  app.use(morgan('production'));
+  app.use(morgan('combined'));
+  app.use(helmet({ contentSecurityPolicy: false }));
+  app.use(hpp());
 } else {
   app.use(morgan('dev'));
 }
